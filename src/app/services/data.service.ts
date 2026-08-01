f69c9238dbc999b39293b63a59fbbd007efba843
NicolaCo <nicolas.collin42@gmail.com>
2026-08-01 16:46:45 +0200

    refacto(service): mouv data to a service

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Olympic } from "../models/olympic.model";
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { catchError, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataService {
+  private olympicUrl: string = './assets/mock/olympic.json';
+  error: string | null = null;
+
+  constructor(private http: HttpClient){
+    
+  }
+  
+  loadOlympics(): Observable<Olympic[]> {
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
+      catchError((err: HttpErrorResponse) => {
+        this.error = err.message;
+        return of([]);
+      })
+    );
+  }
+
+  
+
+}
