0d8e932b17ec3d2c3142ec95d8b4075c1495a163
NicolaCo <nicolas.collin42@gmail.com>
2026-08-14 18:48:49 +0200

    feat(log): remove console.log and add errorhandler

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { provideHttpClient } from '@angular/common/http'    ;
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, ErrorHandler } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { GlobalErrorHandler } from './global-error-handler';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter( routes),
         provideHttpClient(),
+        { provide: ErrorHandler, useClass: GlobalErrorHandler },
     ],
 };
